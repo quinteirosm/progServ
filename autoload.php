ec1edb18eaab75ea42aaed3508134c05799125a4
Miguel Quinteiros <quinteiros.m@gmail.com>
2022-09-29 09:21:30 +0200

    corr ex01

---
+++ autoload.php
@@ -0,0 +1,13 @@
+<?php
+
+// Your custom class dir
+define('SRC_DIR', './src/');
+
+// Add your class dir to include path
+set_include_path(SRC_DIR);
+
+// You can use this trick to make autoloader look for commonly used   "My.class.php" type filenames
+spl_autoload_extensions('.php');
+
+// Use default autoload implementation
+spl_autoload_register();
